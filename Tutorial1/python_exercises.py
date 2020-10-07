2b9a8410b24d9a08d9bdfd4ca984c22546b1697b
Geric <gericpan@umich.edu>
2020-10-07 09:20:57 -0400

    finished

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -3,18 +3,27 @@ Intro to python exercises shell code
 """
 
 def is_odd(x):
-    """
-    returns True if x is odd and False otherwise
-    """
+    if x%2==1:
+        return True
+    return False
 
 
 def is_palindrome(word):
+    for i in range(int(len(word)/2)):
+        if word[i]!=word[len(word)-i-1]:
+            return False
+    return True
     """
     returns whether `word` is spelled the same forwards and backwards
     """
 
 
 def only_odds(numlist):
+    list1 = list()
+    for i in numlist:
+        if i%2==1:
+            list1.append(i)
+    return list1
     """
     returns a list of numbers that are odd from numlist
 
@@ -23,6 +32,14 @@ def only_odds(numlist):
 
 
 def count_words(text):
+    dictionary=dict()
+    word=text.split()
+    for stringin in word:
+        if stringin in dictionary:
+            dictionary[stringin]+=1
+        else:
+            dictionary[stringin]=1
+    return dictionary
     """
     return a dictionary of {word: count} in the text
 
@@ -35,3 +52,5 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+print(only_odds([1, 2, 3, 4, 5, 6]))
+    
